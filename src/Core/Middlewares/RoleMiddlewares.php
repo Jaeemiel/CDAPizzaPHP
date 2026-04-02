c465662dd6b6724c1a45e97efcf26b70424d3734
abc <abc@abc.abc>
2026-04-02 16:19:26 +0200

    feat: add RoleMiddleware and protect routes by role

---
+++ src/Core/Middlewares/RoleMiddlewares.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Core\Middlewares;
+
+use App\Core\Auth;
+use App\Core\Session;
+
+class RoleMiddlewares implements InterfaceMiddlewares {
+
+    public ?array $expected_role = null;
+    public function __construct($expected_role){
+        $this->expected_role = explode(",",$expected_role);
+    }
+
+    public function handle() : void {
+//        var_dump($this->expected_role);
+        $user = Auth::user();
+        if (!in_array($user->role,$this->expected_role)) {
+
+            Session::setFlash("danger", "Vous n'avez pas les droits pour accéder à cette page.");
+            header("Location: /");
+            exit;
+        }
+    }
+}

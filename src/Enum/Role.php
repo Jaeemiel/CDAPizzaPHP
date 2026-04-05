3aaa520f851d5c291c6127d78933e31d08065577
Jaeemiel_REY <jaeemiel@gmail.com>
2026-04-05 23:42:55 +0200

    feat: ajout peutChangerEtat et etatsModifiables dans Role enum

--- src/Enum/Role.php
+++ src/Enum/Role.php
@@ -7,4 +7,28 @@ enum Role: string
     case GUICHET = 'GUICHET';
     case CUISINE = 'CUISINE';
     case GERANT = 'GERANT';
+
+    /**
+     * Retourne les états que ce rôle peut faire avancer.
+     *
+     * @return EtatCommande[]
+     */
+    public function etatsModifiables(): array {
+        return match($this) {
+            self::GUICHET => [EtatCommande::PAYER, EtatCommande::PRETE],
+            self::CUISINE => [EtatCommande::PREPARATION],
+            default       => [],
+        };
+    }
+
+    /**
+     * Vérifie si ce rôle peut changer l'état donné.
+     *
+     * @param EtatCommande $etat
+     * @return bool
+     */
+    public function peutChangerEtat(EtatCommande $etat): bool {
+        return in_array($etat, $this->etatsModifiables());
+    }
+
 }
